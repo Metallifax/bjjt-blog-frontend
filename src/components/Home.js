95411164d08797035a24d53909027fcdbb594e56
Metallifax <mathew_forgione@msn.com>
2022-03-31 00:22:22 -0300

    added some basic editor saving functionality using redux
    will test this using e2e as it's difficult to do black box testing with react-draft-wysiwyg

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,29 @@
-const Home = () => (
-  <>
-    <h1>Home!</h1>
-  </>
-);
+import { useSelector } from 'react-redux';
+
+const Home = () => {
+  const posts = useSelector((state) => state.editor.posts);
+
+  const getPosts = () => {
+    return posts.map((post) => {
+      const textArr = post.getCurrentContent().getPlainText().split(' ', 25);
+
+      return (
+        // eslint-disable-next-line react/jsx-key
+        <li>
+          {textArr.length === 25
+            ? textArr.join(' ').concat('...')
+            : textArr.join(' ')}
+        </li>
+      );
+    });
+  };
+
+  return (
+    <>
+      <h1>Home!</h1>
+      <ul>{getPosts()}</ul>
+    </>
+  );
+};
 
 export default Home;
--- src/components/editor/BlogEditor.js
+++ src/components/editor/BlogEditor.js
@@ -3,7 +3,9 @@ import { EditorState } from 'draft-js';
 import { useEffect, useState } from 'react';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { useDispatch } from 'react-redux';
-import { update } from '../../features/editor/editorSlice';
+import { save, update } from '../../features/editor/editorSlice';
+import { useNavigate } from 'react-router-dom';
+import './BlogEditor.scss';
 
 const BlogEditor = () => {
   const [editorState, setEditorState] = useState(() =>
@@ -11,25 +13,38 @@ const BlogEditor = () => {
   );
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(update(editorState));
   }, [editorState, dispatch]);
 
+  const savePostHandler = () => {
+    dispatch(save(editorState));
+    navigate('/');
+  };
+
   return (
-    <div
-      style={{
-        border: '1px solid black',
-        padding: '2px',
-        minHeight: '400px',
-      }}
-    >
-      <Editor
-        data-testid='editor'
-        editorState={editorState}
-        onEditorStateChange={setEditorState}
-      />
-    </div>
+    <>
+      <div
+        style={{
+          border: '1px solid black',
+          padding: '2px',
+          minHeight: '400px',
+        }}
+      >
+        <Editor
+          data-testid='editor'
+          editorState={editorState}
+          onEditorStateChange={setEditorState}
+        />
+      </div>
+      <div className='btn--container'>
+        <button onClick={savePostHandler} className='btn btn-primary'>
+          Save!
+        </button>
+      </div>
+    </>
   );
 };
 
