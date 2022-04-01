f292b42f00fcd3801fa3f21d2b68a0d67c75d42a
Metallifax <mathew_forgione@msn.com>
2022-03-31 23:35:59 -0300

    Basic styling to blog post on home page and added more inputs to editor form

--- src/components/editor/BlogEditor.jsx
+++ src/components/editor/BlogEditor.jsx
@@ -5,9 +5,13 @@ import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { useDispatch } from 'react-redux';
 import { save, update } from '../../features/editor/editorSlice';
 import { useNavigate } from 'react-router-dom';
+import { Container, Form } from 'react-bootstrap';
 import './BlogEditor.scss';
 
 const BlogEditor = () => {
+  const [title, setTitle] = useState('');
+  const [name, setName] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty(),
   );
@@ -21,31 +25,59 @@ const BlogEditor = () => {
 
   const savePostHandler = () => {
     if (editorState.getCurrentContent().getPlainText()) {
-      dispatch(save(editorState));
+      const post = {
+        name,
+        title,
+        imageUrl,
+        editorState,
+      };
+
+      dispatch(save(post));
       navigate('/');
     }
   };
 
   return (
     <>
-      <div
-        style={{
-          border: '1px solid black',
-          padding: '2px',
-          minHeight: '400px',
-        }}
-      >
-        <Editor
-          data-testid='editor'
-          editorState={editorState}
-          onEditorStateChange={setEditorState}
-        />
-      </div>
-      <div className='btn--container'>
-        <button onClick={savePostHandler} className='btn btn-primary'>
-          Save!
-        </button>
-      </div>
+      <h1 className='text-center m-3'>Create a Blog Post!</h1>
+      <Container className='container--override'>
+        <Form>
+          <Form.Group className='form-group--container'>
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              placeholder='Enter a title for your post!'
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className='form-group--container'>
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              placeholder='Enter your name!'
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className='form-group--container'>
+            <Form.Label>Image Url</Form.Label>
+            <Form.Control
+              placeholder='Enter your image url!'
+              onChange={(e) => setImageUrl(e.target.value)}
+            />
+          </Form.Group>
+        </Form>
+        <div className='editor--container'>
+          <Editor
+            blockStylefn='editor--wrapper'
+            data-testid='editor'
+            editorState={editorState}
+            onEditorStateChange={setEditorState}
+          />
+        </div>
+        <div className='btn--container'>
+          <button onClick={savePostHandler} className='btn btn-primary'>
+            Save!
+          </button>
+        </div>
+      </Container>
     </>
   );
 };
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,27 +1,41 @@
 import { useSelector } from 'react-redux';
+import { Card, Container, Row } from 'react-bootstrap';
 
 const Home = () => {
   const posts = useSelector((state) => state.editor.posts);
 
   const getPosts = (postsArr) => {
     return postsArr.map((post) => {
-      const textArr = post.getCurrentContent().getPlainText().split(' ', 25);
+      const textArr = post.editorState
+        .getCurrentContent()
+        .getPlainText()
+        .split(' ', 25);
 
       return (
-        // eslint-disable-next-line react/jsx-key
-        <li>
-          {textArr.length === 25
-            ? textArr.join(' ').concat('...')
-            : textArr.join(' ')}
-        </li>
+        <Card style={{ width: '18rem', margin: '10px' }} key={post.title}>
+          <Card.Img variant='top' src={post.imageUrl} />
+          <Card.Body>
+            <Card.Title>{post.title}</Card.Title>
+            <Card.Text>
+              {textArr.length === 25
+                ? textArr.join(' ').concat('...')
+                : textArr.join(' ')}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>By: {post.name}</Card.Footer>
+        </Card>
       );
     });
   };
 
   return (
     <>
-      <h1>Home!</h1>
-      <ul>{posts && getPosts(posts)}</ul>
+      <h1 className='text-center m-3'>Home!</h1>
+      <Container>
+        <Row style={{ justifyContent: 'center' }}>
+          {posts && getPosts(posts)}
+        </Row>
+      </Container>
     </>
   );
 };
