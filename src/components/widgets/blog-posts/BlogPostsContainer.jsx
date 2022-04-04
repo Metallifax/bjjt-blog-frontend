ad1faab0e26e1f9938fe566ac3c34bea72d6c8a2
Metallifax <mathew_forgione@msn.com>
2022-04-04 05:47:04 -0300

    Added some more styling and the ability to delete a post from the home page. Closes #4.

--- src/components/widgets/blog-posts/BlogPostsContainer.jsx
+++ src/components/widgets/blog-posts/BlogPostsContainer.jsx
@@ -1,23 +1,18 @@
 import { Container, Row } from 'react-bootstrap';
 
 import BlogPostCard from './blog-post-card/BlogPostCard';
+import './BlogPostsContainer.scss';
 
-const BlogPostsContainer = ({ posts }) => {
+const BlogPostsContainer = ({ posts = [] }) => {
   const getPosts = (postsArr) => {
-    return postsArr.map((post) => (
-      <BlogPostCard
-        title={post.title}
-        name={post.name}
-        imageUrl={post.imageUrl}
-        editorState={post.editorState}
-        key={post.title}
-      />
-    ));
+    return postsArr.map((post) => <BlogPostCard post={post} key={post.id} />);
   };
 
   return (
     <Container>
-      <Row style={{ justifyContent: 'center' }}>{posts && getPosts(posts)}</Row>
+      <Row className='container--row' style={{ marginTop: 10 }}>
+        {posts && getPosts(posts)}
+      </Row>
     </Container>
   );
 };
--- src/components/widgets/blog-posts/blog-post-card/BlogPostCard.jsx
+++ src/components/widgets/blog-posts/blog-post-card/BlogPostCard.jsx
@@ -1,24 +1,42 @@
-import { Card } from 'react-bootstrap';
+import moment from 'moment';
+import { Button, Card } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+
+import { deletePost } from '../../../../features/editor/editorSlice';
 import './BlogPostCard.scss';
 
-const BlogPostCard = ({ title, name, imageUrl, editorState }) => {
-  const textArr = editorState.split(' ', 25);
+const BlogPostCard = ({ post }) => {
+  const textArr = post.editorState.split(' ', 25);
+  const date = post.dateCreated;
+  const dispatch = useDispatch();
+
+  const formatted = moment(date).format('MMMM Do YYYY, h:mm:ss a');
 
   return (
     <Card
       className='card--container'
-      style={{ width: '18rem', margin: '10px' }}
+      style={{
+        display: 'flex',
+        justifyContent: 'flex-start',
+        width: '18rem',
+        margin: '10px',
+        padding: 0,
+      }}
     >
-      <Card.Img className='card--image' variant='top' src={imageUrl} />
+      <Card.Img className='card--image' variant='top' src={post.imageUrl} />
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
+        <Card.Title>{post.title}</Card.Title>
+        <Card.Subtitle className='mb-2 text-muted'>{formatted}</Card.Subtitle>
         <Card.Text>
           {textArr.length === 25
             ? textArr.join(' ').concat('...')
             : textArr.join(' ')}
         </Card.Text>
       </Card.Body>
-      <Card.Footer>By: {name}</Card.Footer>
+      <Card.Footer>
+        <Card.Text>By: {post.name}</Card.Text>
+        <Button onClick={() => dispatch(deletePost(post))}>Delete</Button>
+      </Card.Footer>
     </Card>
   );
 };
