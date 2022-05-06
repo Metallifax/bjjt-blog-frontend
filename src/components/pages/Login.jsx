cb3a449c81195dba571b7a0567db4e7ad0134afc
Metallifax <mathew_forgione@msn.com>
2022-05-06 13:40:15 -0300

    configured client side errors in Login form and added testing for it

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -7,38 +7,80 @@ import FormContainer from '../widgets/custom-forms/FormContainer';
 import FormInput from '../widgets/custom-forms/FormInput';
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [form, setForm] = useState({ email: '', password: '' });
+  const [errors, setErrors] = useState({});
 
-  const logDetails = (event) => {
-    event.preventDefault();
+  const setField = (field, value) => {
+    setForm({
+      ...form,
+      [field]: value,
+    });
 
-    const details = {
-      email,
-      password,
-    };
+    if (errors[field]) {
+      setErrors({
+        ...errors,
+        [field]: null,
+      });
+    }
+  };
 
-    console.log(details);
+  const findFormErrors = () => {
+    const { email, password } = form;
+    const newErrors = {};
+
+    if (!email || email === '') newErrors.email = 'Cannot be blank';
+    if (!password || password === '') newErrors.password = 'Cannot be blank';
+
+    return newErrors;
+  };
+
+  const changeEmailHandler = (e) => {
+    setField('email', e.target.value);
+  };
+
+  const changePasswordHandler = (e) => {
+    setField('password', e.target.value);
+  };
+
+  const submitLogin = (e) => {
+    e.preventDefault();
+
+    const newErrors = findFormErrors();
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+    } else {
+      const details = {
+        email: form.email,
+        password: form.password,
+      };
+
+      console.log(details);
+    }
   };
 
   return (
     <Container className='mt-3'>
       <h1>Login</h1>
-      <FormContainer onSubmit={(e) => logDetails(e)} buttonText='Login'>
+      <FormContainer onSubmit={submitLogin} buttonText='Login'>
         <FormInput
           id='email'
           label='Email'
           placeholder='Enter your email!'
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
+          onChange={(e) => changeEmailHandler(e)}
+          isInvalid={errors.email}
+          errorMessage={errors.email}
+          value={form.email}
           formGroupClass='mt-3'
         />
         <FormInput
           id='password'
           label='Password'
           placeholder='Enter your password!'
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
+          onChange={(e) => changePasswordHandler(e)}
+          isInvalid={errors.password}
+          errorMessage={errors.password}
+          value={form.password}
           type='password'
           formGroupClass='mt-3'
         />
--- src/components/pages/__tests__/Login.spec.js
+++ src/components/pages/__tests__/Login.spec.js
@@ -28,4 +28,80 @@ describe('login page tests', () => {
 
     expect(console.log).toHaveBeenCalled();
   });
+
+  // eslint-disable-next-line max-len
+  test('not filling both the email and password fields yield both errors', () => {
+    localRender();
+
+    const submitButton = screen.getByRole('button', { name: 'Login' });
+
+    fireEvent.click(submitButton);
+
+    const inputs = screen.getAllByTestId('input');
+    expect(inputs[0]).toHaveTextContent('Cannot be blank');
+    expect(inputs[1]).toHaveTextContent('Cannot be blank');
+  });
+
+  // eslint-disable-next-line max-len
+  test('filling the email but leaving the password field empty yields one error', () => {
+    localRender();
+
+    const emailInput = screen.getByLabelText('Email');
+    fireEvent.change(emailInput, { target: { value: 'email' } });
+
+    const submitButton = screen.getByRole('button', { name: 'Login' });
+    fireEvent.click(submitButton);
+
+    const inputs = screen.getAllByTestId('input');
+    expect(inputs[0]).not.toHaveTextContent('Cannot be blank');
+    expect(inputs[1]).toHaveTextContent('Cannot be blank');
+  });
+
+  // eslint-disable-next-line max-len
+  test('filling the password but leaving the email field empty yields one error', () => {
+    localRender();
+
+    const passwordInput = screen.getByLabelText('Password');
+    fireEvent.change(passwordInput, { target: { value: 'password' } });
+
+    const submitButton = screen.getByRole('button', { name: 'Login' });
+    fireEvent.click(submitButton);
+
+    const inputs = screen.getAllByTestId('input');
+    expect(inputs[1]).not.toHaveTextContent('Cannot be blank');
+    expect(inputs[0]).toHaveTextContent('Cannot be blank');
+  });
+
+  test('filling both inputs and pressing submit does not yield errors', () => {
+    localRender();
+
+    const emailInput = screen.getByLabelText('Email');
+    const passwordInput = screen.getByLabelText('Password');
+
+    fireEvent.change(emailInput, { target: { value: 'email' } });
+    fireEvent.change(passwordInput, { target: { value: 'password' } });
+
+    const submitButton = screen.getByRole('button', { name: 'Login' });
+    fireEvent.click(submitButton);
+
+    const inputs = screen.getAllByTestId('input');
+    expect(inputs[1]).not.toHaveTextContent('Cannot be blank');
+    expect(inputs[0]).not.toHaveTextContent('Cannot be blank');
+  });
+
+  // eslint-disable-next-line max-len
+  test('after triggering error then re-entering text, errors go away', () => {
+    localRender();
+
+    const submitButton = screen.getByRole('button', { name: 'Login' });
+    fireEvent.click(submitButton);
+
+    const inputs = screen.getAllByTestId('input');
+    expect(inputs[0]).toHaveTextContent('Cannot be blank');
+
+    const input = screen.getByLabelText('Email');
+    fireEvent.change(input, { target: { value: 'hello' } });
+
+    expect(inputs[0]).not.toHaveTextContent('Cannot be blank');
+  });
 });
